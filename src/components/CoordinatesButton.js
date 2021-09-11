25d23967ebc57f35d8dfda424f1b7053884f3035
John M. Stassen <john.stassen@gmail.com>
2021-09-11 13:27:53 -0700

    commit tree b5aed160360bf922ca90525cb9e0c5039385cd8c

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React from 'react'
+
+export default class CoordinatesButton extends React.Component {
+  
+    handleClick = (event) => {
+      const callback = this.props.onReceiveCoordinates
+      const coordinates = [event.clientX, event.clientY]
+  
+      callback(coordinates)
+    }
+  
+    render() {
+      return(
+        <button onClick={this.handleClick}></button>
+      )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,15 @@
 // Code DelayedButton Component Here
+import React from 'react'
+
+export default class DelayedButton extends React.Component {
+  render() {
+    return(
+      <button onClick={this.handleClick}></button>
+    )
+  }
+  
+  handleClick = (e) => {
+    e.persist()
+    window.setTimeout(this.props.onDelayedClick(e), this.props.delay)
+  }
+}
